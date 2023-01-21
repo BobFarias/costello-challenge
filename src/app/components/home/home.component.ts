75b68b1d2e644aa395b8690299894ebe6710610c
Bob Farias <bobhenrique9@gmail.com>
2023-01-21 13:30:59 +0000

    [ADD] html structure for the pagination [ADD] logic to render the table according to the page [FIX] decrease the height of the table

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,7 +1,10 @@
-import { Component, OnInit } from '@angular/core';
+import { ChangeDetectorRef, Component, OnInit, ViewChild } from '@angular/core';
+import { MatPaginator } from '@angular/material/paginator';
+import { MatTableDataSource } from '@angular/material/table';
+import { Observable } from 'rxjs';
 import { CtgovApiService } from 'src/app/services/ctgov-api.service';
 
-import { CTGovItem, CTGovResponse } from './interfaces';
+import { CTGovItem } from './interfaces';
 
 @Component({
   selector: 'app-home',
@@ -10,16 +13,41 @@ import { CTGovItem, CTGovResponse } from './interfaces';
 })
 
 export class HomeComponent implements OnInit {
-  dataSource = undefined;
+  tableData = undefined;
+  
+  dataSource: MatTableDataSource<CTGovItem>;
+  @ViewChild(MatPaginator, { static: true }) paginator: MatPaginator;
+  dataObs$: Observable<any>;
+
+
   displayedColumns: string[] = ['#', 'NCTId', 'Title', 'Condition', 'Status', 'Primary Completion Date'];
 
+  pageIndex:number;
+  pageSize:number;
+  length:number;
+
   constructor(
-    private ctgovService: CtgovApiService
+    private ctgovService: CtgovApiService,
+    private _changeDetectorRef: ChangeDetectorRef
   ) { }
 
-  ngOnInit(): void {
+  public getTrialData(){
     this.ctgovService.trials$.subscribe(trials => {
-      this.dataSource = trials;
+      //UPDATE TABLE DATA WITH ALL THE ARRAYS
+      this.tableData = trials;
+
+      //UPDATE THE STATE WHICH WILL SHOW ON THE TABLE ACCORDING TO THE PAGINATION
+      this.dataSource = new MatTableDataSource<CTGovItem>(trials);
+      
+      //PAGINATION CONFIG
+      this.length = trials.length;
+      this._changeDetectorRef.detectChanges();
+      this.dataSource.paginator = this.paginator;
+      this.dataObs$ = this.dataSource.connect();
     });
-   }
+  }
+
+  ngOnInit(): void {
+    this.getTrialData();
+  }
 }
