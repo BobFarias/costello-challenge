2b89637cfe6c6efbd61459ab2273f3fcd7ddb398
Bob Farias <bobhenrique9@gmail.com>
2023-01-21 03:23:19 +0000

    [ADD] declared all the states from the component filters [ADD] all the functions to update the states

--- src/app/components/home/filters/filters.component.ts
+++ src/app/components/home/filters/filters.component.ts
@@ -2,7 +2,10 @@ import { Component, OnInit } from '@angular/core';
 import { CtgovApiService } from 'src/app/services/ctgov-api.service';
 import {FormGroup, FormControl} from '@angular/forms';
 
-
+import * as moment from 'moment';
+interface IStatusFilter {
+  value: string;
+}
 @Component({
   selector: 'app-filters',
   templateUrl: './filters.component.html',
@@ -14,20 +17,46 @@ export class FiltersComponent implements OnInit {
     end: new FormControl<Date | null>(null),
   });
 
+  //NUMBER OF TRIASL
   numTrials = 0;
+
+  //STATES FOR THE FILTERS
+  status: IStatusFilter[] = [
+    {value: 'Recruiting'},
+    {value: 'Active'},
+    {value: 'Completed'},
+    {value: 'Unknown'},
+  ];
+  
+  condition: "";
+  selectedStatus: string;
+  primaryCompletionDateFrom: ""; // Takes format 'YYYY/MM/DD'
+  primaryCompletionDateUntil: ""; // Takes format 'YYYY/MM/DD'
+
 
   constructor(
     private ctgovService: CtgovApiService
   ) { }
 
-  ngOnInit(): void {
-    this.ctgovService.loadTrialData();  
-  }
-
   getTrialData() {
+    const formatedDate = (date: string) => {
+      if(!date) return null;
+      return moment(date).format("YYYY-MM-DD")
+    };
+
+    this.ctgovService.loadTrialData(
+      this.selectedStatus,
+      this.condition,
+      formatedDate(this.primaryCompletionDateFrom),
+      formatedDate(this.primaryCompletionDateUntil)
+    );  
     this.ctgovService.numTrials$.subscribe(numTrials => {
       this.numTrials = numTrials;
     });
-  }
 
+   }
+
+  ngOnInit(): void {
+    this.getTrialData();
+  }
 }
